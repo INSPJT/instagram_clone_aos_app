2b16edb61c2de95b6ecec7077d4826d0be19b8d5
김주영 <2013104065@khu.ac.kr>
2021-03-23 22:21:12 +0900

    login

---
+++ InstagramClone/app/src/main/java/com/cookandroid/instagramclone/LoginActivity.kt
@@ -0,0 +1,84 @@
+package com.cookandroid.instagramclone
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class LoginActivity : AppCompatActivity() {
+    val url = "http://13.209.101.178:80" // 접속 url
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val logger = OkHttpClient.Builder().addInterceptor(interceptor).readTimeout(20,TimeUnit.SECONDS).writeTimeout(20,TimeUnit.SECONDS).build()
+
+        val retrofit = Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).client(logger).build()
+
+
+        val retrofitService = retrofit.create(LoginService::class.java)
+
+        login_btn.setOnClickListener {
+            val text_email = input_email.text.toString()
+            val text_pw = input_pw.text.toString()
+            val Body = HashMap<String,String>()
+
+            Body.put("email",text_email)
+            Body.put("password",text_pw)
+
+            retrofitService.Login(Body).enqueue(object : Callback<User>{
+                override fun onResponse(call: Call<User>, response: Response<User>) { // 통신 성공
+                    when(response.code()){
+                        200 -> { // 성공
+                            val accesstoken = response.body()!!.accessToken
+                            val accesstokenexpirein = response.body()!!.accessTokenExpiresIn
+                            val granttype = response.body()!!.grantType
+                            val refreshtoken = response.body()!!.refreshToken
+
+                            val user = User(text_email,text_pw,accesstoken,accesstokenexpirein,granttype,refreshtoken,"","")
+
+                            Toast.makeText(this@LoginActivity,"로그인 성공",Toast.LENGTH_LONG).show()
+                            Log.d("성공","성공")
+
+                            val intent = Intent(this@LoginActivity,MainActivity::class.java)
+                            intent.putExtra("user",user)
+                            startActivity(intent)
+
+                        }
+                        401 -> { // 로그인 실패
+                            Toast.makeText(this@LoginActivity,"회원가입이 필요",Toast.LENGTH_LONG).show()
+                            Log.d("code",response.code().toString())
+                        }
+                        404 -> { // 서버 오류
+                            Toast.makeText(this@LoginActivity,"서버 이상",Toast.LENGTH_LONG).show()
+                            Log.d("code",response.code().toString())
+                        }
+                    }
+
+                }
+
+                override fun onFailure(call: Call<User>, t: Throwable) {
+                    Toast.makeText(this@LoginActivity,"로그인 실패",Toast.LENGTH_LONG).show()
+                    Log.d("실패 로그",t.toString())
+                }
+            })
+
+        }
+
+
+    }
+}
--- InstagramClone/app/src/main/java/com/cookandroid/instagramclone/User.kt
+++ InstagramClone/app/src/main/java/com/cookandroid/instagramclone/User.kt
@@ -1,9 +1,52 @@
 package com.cookandroid.instagramclone
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class User(
-    var Email : String,
-    var Password : String,
-    var AccessToken :String,
-    var Nickname : String,
-    var ExpiresTime : String
-)
+    var email: String?,
+    var password: String?,
+    var accessToken: String?,
+    var accessTokenExpiresIn: Long,
+    var grantType: String?,
+    var refreshToken: String?,
+    var nickname: String?,
+    var expiresTime: String?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readLong(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeString(email)
+        dest?.writeString(password)
+        dest?.writeString(accessToken)
+        dest?.writeLong(accessTokenExpiresIn)
+        dest?.writeString(grantType)
+        dest?.writeString(refreshToken)
+        dest?.writeString(nickname)
+        dest?.writeString(expiresTime)
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
