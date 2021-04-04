a253aa295689cf7baab7aab68ec14db33d1af6cf
sangdo913 <sangdo913@gmail.com>
2021-04-05 00:58:37 +0900

    latest update

--- InstagramClone/app/src/main/java/com/cookandroid/instagramclone/AddPhotoActivity.kt
+++ InstagramClone/app/src/main/java/com/cookandroid/instagramclone/AddPhotoActivity.kt
@@ -1,14 +1,108 @@
 package com.cookandroid.instagramclone
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.text.Layout
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.activity_add_photo.*
+import kotlinx.android.synthetic.main.activity_add_photo.view.*
+import java.io.File
+import java.io.FileInputStream
 
 class AddPhotoActivity : AppCompatActivity() {
+    private fun getPathOfAllImages(): ArrayList<String> {
+        var result: ArrayList<String> = ArrayList()
+        var uri: Uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        val cursor = getContentResolver().query(
+            uri,
+            null,
+            null,
+            null,
+            MediaStore.MediaColumns.DATE_ADDED + " desc"
+        )
+        if (cursor != null) {
+            while (cursor.moveToNext())
+                result.add(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)))
+            cursor.close()
+        }
+        return result
+    }
+
+    private fun checkPermissions() {
+        var writePermission =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        var readPermission =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        var cameraPermission =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        if (writePermission == PackageManager.PERMISSION_DENIED || readPermission == PackageManager.PERMISSION_DENIED || cameraPermission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                ),
+                100
+            )
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_add_photo)
-        //setContentView(R.layout.post_explanation)
-        setContentView(R.layout.main_navigation)
+        setContentView(R.layout.activity_add_photo)
+        checkPermissions()
+        val imageList = getPathOfAllImages()
+        addPhotoRecyclerView.adapter = UserFragmentRecyclerViewAdapter(imageList)
+        addPhotoRecyclerView.layoutManager = GridLayoutManager(this, 3)
+    }
+
+    inner class UserFragmentRecyclerViewAdapter(private var images: ArrayList<String>) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var width = resources.displayMetrics.widthPixels / 3
+            var imageView = ImageView(parent.context)
+            imageView.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            return CustomViewHolder(imageView)
+        }
+
+        inner class CustomViewHolder(var imageView: ImageView) :
+            RecyclerView.ViewHolder(imageView) {
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var imageview = (holder as CustomViewHolder).imageView
+            var file = File(images[position])
+            Glide.with(holder.itemView.context).load(file).apply(RequestOptions().centerCrop()) .into(imageview)
+        }
+
+        override fun getItemCount(): Int {
+            return images.size
+        }
     }
 }
+
---
+++ InstagramClone/app/src/main/java/com/cookandroid/instagramclone/LoginService.kt
@@ -0,0 +1,15 @@
+package com.cookandroid.instagramclone
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface LoginService {
+
+    @Headers("accept: application/json",
+        "content-type: application/json")
+    //@FormUrlEncoded -> Body 쓸땐 이걸 쓰면 오류난다. -> Field쓸때만 쓰면 될듯
+    @POST("/auth/signin") // url
+    fun Login(
+        @Body params : HashMap<String,String>
+    ) : Call<User>
+}
