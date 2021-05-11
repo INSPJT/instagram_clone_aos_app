a1d35554d7ced24b689710ae66aa0bb08352779c
김주영 <37646197+JooYoung1121@users.noreply.github.com>
2021-05-11 13:00:49 +0900

    Revert "Sangdo"

--- InstagramClone/app/src/main/java/com/cookandroid/instagramclone/MainActivity.kt
+++ InstagramClone/app/src/main/java/com/cookandroid/instagramclone/MainActivity.kt
@@ -8,6 +8,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_add_photo)
+        //setContentView(R.layout.post_explanation)
 
         setContentView(R.layout.posting)
 
